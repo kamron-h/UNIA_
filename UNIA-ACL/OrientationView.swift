ea01ad9c386d67d895a8bcc15e202e9af06dfaf8
Kamron H <19638873+kamronthopkins@users.noreply.github.com>
2022-01-23 15:30:08 -0500

    Created OrientationView & Populated w/ Code
    Used a List to display the weeks and names of the UNIA orientation lessons.

--- UNIA-ACL/OrientationView.swift
+++ UNIA-ACL/OrientationView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct OrientationView: View {
+    
+    let weeks = [
+        "Week 1 - Intro to UNIA",
+        "Week 2 - Garvey's Legacy",
+        "Week 3 - UNIA Fundamentals",
+        "Week 4 - World History",
+        "Week 5 - UNIA Politics",
+        "Week 6 - The African Legion",
+        "Week 7 - The BlackCross",
+        "Week 8 - Black Economics",
+        "Week 9 - African Mathematics",
+        "Week 10 - African History",
+        "Week 11 - Current Opposition",
+        "Week 12 - The UNIAs Future"]
+    
     var body: some View {
-        Text("Welcome to Orientation")
+        ScrollView {
+            Spacer()
+            ForEach(weeks, id: \.self) { week in
+                NavigationLink {
+                    NewsView()
+                } label: {
+                    Text(week)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.orange)
+                }
+                .frame(maxWidth: .infinity)
+                .padding([.leading, .top], 5)
+                Divider()
+//                Text(week)
+            }
+        }
     }
 }
 
