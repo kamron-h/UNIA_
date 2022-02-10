2354cd3f4290bf3e7b46eede6ff179f96e1fb5b5
Kamron H <19638873+kamronthopkins@users.noreply.github.com>
2022-02-10 14:00:05 -0500

    Placed DirectoryView Inside A Dedicated Folder

--- UNIA-ACL/Directory/DirectoryView.swift
+++ UNIA-ACL/Directory/DirectoryView.swift
@@ -49,7 +49,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -63,7 +63,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -77,7 +77,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -91,7 +91,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -109,7 +109,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -123,7 +123,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -141,7 +141,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -155,7 +155,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -169,7 +169,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -183,7 +183,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -201,7 +201,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -215,7 +215,7 @@ struct DirectoryView: View {
                     email: "GaveyM@mail.com",
                     number: "1(999)333-5555",
                     website: "Unia-Acl_government.com",
-                    certifications: ["Agri-1", "Agri-2", "Agri-1"],
+                    certifications: ["Unity Building", "African Empowerment", "Fighting Oppression"],
                     desgrees: ["Degree-1", "Degree-2", "Degree-1"],
                     skills: ["Skill-1", "Skill-2", "Skill-1"],
                     experience: "One or more paragraph's of information",
@@ -225,8 +225,6 @@ struct DirectoryView: View {
             ])
         
     ]
-    
-    
     
     
     
@@ -258,22 +256,6 @@ struct DirectoryView: View {
     ]
     @State private var singleSelection : UUID?
     
-    
-    
-    
-    
-    
-    
-    
-    let directoryList : [String] = [
-        
-        "Hello, World!",
-        "Hello, World!",
-        "Hello, World!",
-        "Hello, World!",
-        "Hello, World!",
-    ]
-    
     let skills : [String] = [
         
         "Accounting",
@@ -285,154 +267,110 @@ struct DirectoryView: View {
         "Music Production",
     ]
     
+    @State var usDivision = ""
     
     var body: some View {
         
-        List(selection: $singleSelection){
-            ForEach(uniaSectors) { sector in
-                Section(header: Text("\(sector.sectorType)")) {
-                    ForEach(sector.members) { member in
-                        
-                        NavigationLink(
-                            member.name,
-                            destination: DirectoryDetailView(
-                                sector: member.sector,
-                                name: member.name,
-                                title: member.title,
-                                email: member.email,
-                                number: member.number,
-                                website: member.website,
-                                certifications: member.certifications,
-                                desgrees: member.desgrees,
-                                skills: member.skills,
-                                experience: member.experience,
-                                creationDate: member.creationDate,
-                                userImageURL: member.userImageURL,
-                                userApproved: member.userApproved)
-                        )
-                        
+        if usDivision == "" {
+            
+            List(selection: $singleSelection){
+                ForEach(uniaSectors) { sector in
+                    Section(header: Text("\(sector.sectorType)")) {
+                        ForEach(sector.members) { member in
+                            
+                            NavigationLink(
+                                member.name,
+                                destination: DirectoryDetailView(
+                                    sector: member.sector,
+                                    name: member.name,
+                                    title: member.title,
+                                    email: member.email,
+                                    number: member.number,
+                                    website: member.website,
+                                    certifications: member.certifications,
+                                    desgrees: member.desgrees,
+                                    skills: member.skills,
+                                    experience: member.experience,
+                                    creationDate: member.creationDate,
+                                    userImageURL: member.userImageURL,
+                                    userApproved: member.userApproved)
+                            )
+                            
+                        }
                     }
                 }
             }
+            
+        } else {
+            
+            //Display sortedDivision if not empty
+            List(selection: $singleSelection){
+                ForEach(uniaSectors) { sector in
+                    
+                    ForEach(sector.members) { member in
+                        
+                        Text("Replace w/ NavigationLink")
+                        Text(member.title)
+                        
+//                        NavigationLink(
+//                            member.name,
+//                            destination: DirectoryDetailView(
+//                                sector: member.sector,
+//                                name: member.name,
+//                                title: member.title,
+//                                email: member.email,
+//                                number: member.number,
+//                                website: member.website,
+//                                certifications: member.certifications,
+//                                desgrees: member.desgrees,
+//                                skills: member.skills,
+//                                experience: member.experience,
+//                                creationDate: member.creationDate,
+//                                userImageURL: member.userImageURL,
+//                                userApproved: member.userApproved)
+//                        )
+
+                        
+                        
+                        
+                    }
+                    
+//                    Section(header: Text("\(sector.sectorType)")) {
+//                        ForEach(sector.members) { member in
+//
+//                            NavigationLink(
+//                                member.name,
+//                                destination: DirectoryDetailView(
+//                                    sector: member.sector,
+//                                    name: member.name,
+//                                    title: member.title,
+//                                    email: member.email,
+//                                    number: member.number,
+//                                    website: member.website,
+//                                    certifications: member.certifications,
+//                                    desgrees: member.desgrees,
+//                                    skills: member.skills,
+//                                    experience: member.experience,
+//                                    creationDate: member.creationDate,
+//                                    userImageURL: member.userImageURL,
+//                                    userApproved: member.userApproved)
+//                            )
+//
+//                        }
+//                    }
+                }
+            }
+
+            .navigationTitle("UNIA Directory")
+            .toolbar { EditButton() }
         }
-        .navigationTitle("UNIA Directory")
-        .toolbar { EditButton() }
         
-//        List(directoryList , id: \.self) { item in
-//        List() {
-//
-//            Section(header: Text("Accounting")) {
-//                Text("Accounting - Assata Shakur (Active)")
-//                Text("Finacial Planning - Amun Ra")
-//                Text("Taxes - Harriet Tubman (Active)")
-//
-//            }
-//            Section(header: Text("Electricity")) {
-//                Text("Commercial - Amun Ra")
-//                Text("Residential - Amun Ra")
-//                Text("Contractor - Amun Ra")
-//            }
-//            Section(header: Text("Engineering")) {
-//                Text("Machincal Enginerring - Malcom X (Active)")
-//                Text("Software Enginerring - Idi Amin")
-//                Text("Software Enginerring - Thomas Sankara (Active)")
-//                Text("Software Enginerring - Patrice Lumumba (Active)")
-//            }
-//            Section(header: Text("Media")) {
-//                Text("Digital Films - Thomas Sankara (Active)")
-//                Text("Digital Art - Thomas Sankara")
-//                Text("Digital Marketing - Thomas Sankara (Active)")
-//                Text("Digital PR - Thomas Sankara")
-//            }
-//            Section(header: Text("Auto Mechanic")) {
-//                Text("Auto - Thomas (Active)")
-//                Text("Electric Media - Thomas")
-//                Text("Commerical Trucks - Thomas (Active)")
-//                Text("Boats - Thomas")
-//            }
-//            Section(header: Text("Nurses")) {
-//                Text("Traveling - Thomas")
-//                Text("Home Health - Thomas")
-//                Text("Pediatric - Thomas")
-//                Text("Hospice - Thomas")
-//            }
-//            Section(header: Text("Plumbing")) {
-//                Text("Commercial - Thomas Sankara")
-//                Text("Residential - Thomas")
-//                Text("Contractor - Thomas")
-//            }
-//            Section(header: Text("Sciences")) {
-//                Text("Biologist - Thomas")
-//                Text("Oceanography - Thomas")
-//                Text("Nurology - Thomas")
-//                Text("Chemist - Thomas")
-//            }
-//
-//
-//
-//        }
-//        .listStyle(.grouped)
-//        .preferredColorScheme(.dark)
+        
+        
     }
 }
 
-struct DirectoryDetailView: View {
-    
-//    @Binding var id : String
-    @State var sector : String
-    @State var name : String
-    
-    @State var title : String
-    
-    @State var email : String
-    @State var number : String
-    @State var website : String
-    @State var certifications : [String]
-    @State var desgrees : [String]
-    @State var skills : [String]
-    @State var experience : String
-    @State var creationDate : Date //Maybe timestamp
-    @State var userImageURL : String
-    @State var userApproved : Bool
-    
-    @State private var certs = [String()]
-    @State private var dgrees = [String()]
-    
-    var body: some View {
-        ScrollView(.vertical) {
-            VStack{
-                Image(userImageURL)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-            }
-            VStack {
-                Text(name)
-                Text(title)
-                Text(email)
-            }
-            VStack {
-                Text(number)
-                Text(website)
-            }
-            VStack {
-//                Text(certifications)
-                ForEach(certifications, id: \.self) { cert in
-                    Text(cert)
-                    
-                }
-                ForEach(desgrees, id: \.self) { degree in
-                    Text(degree)
-                    
-                }
-            }
-            VStack {
-                Text(skills[0])
-                Text(experience)
-            }
-        }
-    }
-}
+
 
 
 
