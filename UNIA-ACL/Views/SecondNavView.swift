ec470a8ca6f098ee6928792c901814fc309d772b
Kamron H <19638873+kamronthopkins@users.noreply.github.com>
2022-02-15 18:35:57 -0500

    Updated SecondNavView UI to Handle SignOut

--- UNIA-ACL/Views/SecondNavView.swift
+++ UNIA-ACL/Views/SecondNavView.swift
@@ -6,13 +6,18 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct SecondNavView: View {
+    
+    let firebaseAuth = Auth.auth()
     
     let gridLayout = [
         GridItem(.flexible()),
         GridItem(.flexible()),
     ]
+    
+    @State private var showSignInView = false
     
     var body: some View {
         
@@ -24,22 +29,45 @@ struct SecondNavView: View {
                     
                     NavigationLink(destination: DivisionsView()) {
                         NavWidget(imageUrl: "garvey", navTitle: "Pay Your Dues ")
+                            .padding([.leading], 5.0)
                     }
                     NavigationLink(destination: HistoryView()) {
                         NavWidget(imageUrl: "AL_1", navTitle: "Fundraisers ")
+                            .padding([.trailing], 5.3)
                     }
                     NavigationLink(destination: NewsView()) {
                         NavWidget(imageUrl: "negroWorld_5", navTitle: "Legislation ")
+                            .padding([.leading], 5.0)
                     }
                     NavigationLink(destination: DivisionsView()) {
                         NavWidget(imageUrl: "AL_14", navTitle: "Submit An Idea ")
+                            .padding([.trailing], 5.3)
                     }
                     NavigationLink(destination: DivisionsView()) {
                         NavWidget(imageUrl: "garvey1", navTitle: "ACL Vote ")
+                            .padding([.leading], 5.0)
                     }
-                    NavigationLink(destination: DivisionsView()) {
+                    
+                    
+                    Button {
+                        //If the member is logged in, sign them out.
+                        if Auth.auth().currentUser != nil {
+                            do {
+                                try firebaseAuth.signOut()
+                                print("Member signed out\n")
+                                self.showSignInView = true
+                            } catch let signOutError as NSError {
+                                print("Error signing out: \n", signOutError)
+                            }
+                        }
+                        
+                    } label: {
                         NavWidget(imageUrl: "unia-acl", navTitle: "Sign In/Out ")
+                            .padding([.trailing], 5.3)
                     }
+                    NavigationLink(destination: Sign_InView(), isActive: $showSignInView) { EmptyView() }
+
+
                 }
             }
 //                .navigationBarHidden(true)
